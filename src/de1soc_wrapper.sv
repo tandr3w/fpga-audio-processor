a82e5a9a989f32d4a9341d87f34c37127807d23c
Andrew <heyitsat12@gmail.com>
2026-02-17 18:59:52 -0500

    Instantiate modules and connect with audio controller

--- src/de1soc_wrapper.sv
+++ src/de1soc_wrapper.sv
@@ -1,4 +1,7 @@
 `timescale 1ns/100ps
+`include "Audio_Controller/Audio_Controller.v"
+`include "avconf/avconf.v"
+`include "top.sv"
 
 module de1soc_wrapper (
     input         CLOCK_50,
@@ -24,9 +27,20 @@ module de1soc_wrapper (
     output        VGA_VS,
     output        VGA_BLANK_N,
     output        VGA_SYNC_N,
-    output        VGA_CLK
-);
+    output        VGA_CLK,
 
+    // Audio CODEC
+    inout         AUD_ADCLRCK, // ADC LR Clock
+    input         AUD_ADCDAT,  // ADC Data
+    inout         AUD_DACLRCK, // DAC LR Clock
+    output        AUD_DACDAT,  // DAC Data
+    inout         AUD_BCLK,    // Bit Clock
+    output        AUD_XCK,     // Chip Clock
+
+    // I2C for Audio Config
+    output        FPGA_I2C_SCLK,
+    inout         FPGA_I2C_SDAT
+);
     // Default Assignments
 
     // LEDs off
@@ -48,5 +62,58 @@ module de1soc_wrapper (
     assign VGA_BLANK_N = 1'b1;
     assign VGA_SYNC_N  = 1'b0;
     assign VGA_CLK     = CLOCK_50;
+
+    wire [31:0] mic_to_logic_L, mic_to_logic_R; // port to receive input from logic
+    wire [31:0] logic_to_spk_L, logic_to_spk_R; // port to send output to driver
+    wire audio_in_available, audio_out_allowed;
+    wire read_request, write_request;
+
+    avconf cfg (
+        .CLOCK_50      (CLOCK_50),
+        .reset         (~KEY[0]),       // Reset when button 0 is pressed
+        .FPGA_I2C_SCLK (FPGA_I2C_SCLK), // Connections to audio chip
+        .FPGA_I2C_SDAT (FPGA_I2C_SDAT) 
+    );
+
+    Audio_Controller driver (
+        .CLOCK_50                (CLOCK_50),
+        .reset                   (~KEY[0]),     // Reset when button 0 is pressed
+
+        .clear_audio_in_memory(1'b0),  
+        .clear_audio_out_memory(1'b0),
+
+        .audio_in_available(audio_in_available),
+        .audio_out_allowed(audio_out_allowed),
+
+        .read_audio_in           (read_request), // Only read and write when DE1-SOC signals availability
+        .write_audio_out         (write_request),
+
+        // Connect audio channels to effect generator
+        .left_channel_audio_in   (mic_to_logic_L),
+        .right_channel_audio_in  (mic_to_logic_R),
+        .left_channel_audio_out  (logic_to_spk_L),
+        .right_channel_audio_out (logic_to_spk_R),
+        
+        // Connections to Audio CODEC
+        .AUD_ADCDAT              (AUD_ADCDAT),
+        .AUD_DACDAT              (AUD_DACDAT),
+        .AUD_BCLK                (AUD_BCLK),
+        .AUD_ADCLRCK             (AUD_ADCLRCK),
+        .AUD_DACLRCK             (AUD_DACLRCK),
+        .AUD_XCK                 (AUD_XCK)
+    );
+
+    top effects (
+        .CLOCK_50   (CLOCK_50),
+        .switches   (SW),
+        .audio_in_L (mic_to_logic_L),
+        .audio_in_R (mic_to_logic_R),
+        .audio_out_L(logic_to_spk_L),
+        .audio_out_R(logic_to_spk_R),
+        .audio_in_available(audio_in_available),
+        .audio_out_allowed(audio_out_available),
+        .read_audio_in(read_request),               
+        .write_audio_out(write_request)          
+    );
 
 endmodule
--- src/top.sv
+++ src/top.sv
@@ -2,13 +2,42 @@
 
 module top #(
 ) (
-    input  logic       a,
-    input  logic       b,
-    output logic       and_o,
-    output logic       xor_o,
-    output logic [2:0] and_or_xor
+    input  logic       CLOCK_50,
+    input  logic [9:0] SW,
+    input  logic       audio_in_available,
+    input  logic       audio_out_allowed, 
+    output logic       read_audio_in,   
+    output logic       write_audio_out, 
+
+    input  logic signed [31:0] audio_in_L,
+    input  logic signed [31:0] audio_in_R,
+    output logic signed [31:0] audio_out_L,
+    output logic signed [31:0] audio_out_R
 );
-    assign and_o = a & b;
-    assign xor_o = a ^ b;
-    assign and_or_xor = {a & b, a | b, a ^ b};
+
+logic signed [31:0] l_processed, r_processed;
+
+assign read_audio_in = audio_in_available && audio_out_allowed;
+assign write_audio_out = audio_in_available && audio_out_allowed;
+
+always_ff @(posedge CLOCK_50) begin
+    if (read_audio_in) begin
+        // Get audio input
+        l_processed = audio_in_L;
+        r_processed = audio_in_R;
+
+        // Apply effects based on switches
+        if (SW[0]) begin
+            l_processed = 0;
+            r_processed = 0;
+        end
+
+        // Output processed audio channels
+        audio_out_L <= l_processed;
+        audio_out_R <= r_processed;
+    end
+end
+
+
+
 endmodule
